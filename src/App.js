bcc51e5b2e58911cde71695ed0d6c263eb935244
Yan Rodrigues <yanrodriguesfm@gmail.com>
2023-06-20 19:23:23 -0300

    incluindo context, data provider e navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-//import Login from '../src/pages/Login';
+import Login from '../src/pages/Login';
 import Home from '../src/pages/Home';
+import DataProvider from './src/components/DataProvider';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <View style={styles.container}>
-      
-      <Home />
+
+      <DataProvider >
+        <NavigationContainer>
+          <Stack.Navigator>
+          <Stack.Scream name="Home" component={Home}/>
+          <Stack.Scream name="Login" component={Login}/>
+          
+          </Stack.Navigator>
+        </NavigationContainer>
+
+      </DataProvider>
+
     </View>
   );
 };
---
+++ src/context/DataContext.js
@@ -0,0 +1,29 @@
+import React, { createContext, useState} from "react";
+import jwt_decode from 'jwt-decode';
+
+export const DataContext = createContext({});
+
+export const DataProvider = ({children}) => {
+   const [dadosUsuario, setDadosUsuario] = useState('');
+
+   const saveDadosUsuario = (jwt) => {
+        var jwtDecodificado = jwt_decode(jwt);
+        var usuario = jwtDecodificado.user;
+        usuario = JSON.parse(usuario);
+
+
+            setDadosUsuario({
+            id: usuario?.id,
+            nome: usuario?.username,
+            email: usuario?.email,
+            token: jwt
+            })
+
+   }
+   return (
+      <DataContext.Provider value={{dadosUsuario, saveDadosUsuario}}>
+         {children}
+      </DataContext.Provider>
+   )
+
+}
